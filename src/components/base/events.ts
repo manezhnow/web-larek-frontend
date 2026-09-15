4a0facf2aee0a3cda91f705f6eb7f72ccb67f38b
Roman Manezhnov <manezhnow@gmail.com>
2026-09-16 01:29:01 +0300

    refactor(base): add types to the event emitter
    - replace the Function type with a typed subscriber
    - simplify the unsubscribe method

--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -1,7 +1,7 @@
 // Хорошая практика даже простые типы выносить в алиасы
 // Зато когда захотите поменять это достаточно сделать в одном месте
 type EventName = string | RegExp;
-type Subscriber = Function;
+type Subscriber = (data: unknown) => void;
 type EmitterEvent = {
     eventName: string,
     data: unknown
@@ -39,9 +39,10 @@ export class EventEmitter implements IEvents {
      * Снять обработчик с события
      */
     off(eventName: EventName, callback: Subscriber) {
-        if (this._events.has(eventName)) {
-            this._events.get(eventName)!.delete(callback);
-            if (this._events.get(eventName)?.size === 0) {
+        const subscribers = this._events.get(eventName);
+        if (subscribers) {
+            subscribers.delete(callback);
+            if (subscribers.size === 0) {
                 this._events.delete(eventName);
             }
         }
